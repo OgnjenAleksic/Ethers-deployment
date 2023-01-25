96b6ef14abd305379319faeebe5a620a57ea58bd
Ognjen <cryptognjen@gmail.com>
2023-01-25 16:04:10 +0100

    change in the comment

--- myContract.sol
+++ myContract.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.7;
 contract myContract {
     uint256 private value;
 
+    //changed for pushing practices
     function addValue(uint256 newValue) public {
         require(newValue >= 10, "Value has to be greater than 10");
         value = newValue;
